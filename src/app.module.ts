4b89722b5c406e2e24c89540cb954372cee74ded
LuisCamus19 <u202319510@upc.edu.pe>
2026-05-21 18:23:21 -0500

    archivo .env y modificacion del app module para conexion con neon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 import { PropertyModule } from './infrastructure/persistence/property.module';
-import { AuthModule } from './infrastructure/auth/auth.module'; // Importante
+import { AuthModule } from './infrastructure/auth/auth.module';
 import { UsersModule } from './infrastructure/persistence/users.module';
 import { WhatsappModule } from './infrastructure/modules/whatsapp.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '77500339',
-      database: 'tunuevocuarto_db',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
+      ssl:
+        process.env.DB_HOST && !process.env.DB_HOST.includes('localhost')
+          ? { rejectUnauthorized: false }
+          : false,
     }),
     PropertyModule,
     AuthModule,
