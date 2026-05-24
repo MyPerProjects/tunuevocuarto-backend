641bed79d36c33133337bd0f00865d97e58c2a13
LuisCamus19 <u202319510@upc.edu.pe>
2026-05-23 21:00:04 -0500

    fix: se agregó ! para evitar error de lectura en js

--- src/infrastructure/auth/google.strategy.ts
+++ src/infrastructure/auth/google.strategy.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@nestjs/common';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       callbackURL:
         process.env.GOOGLE_CALLBACK_URL ||
         'http://localhost:3000/auth/google/callback',
